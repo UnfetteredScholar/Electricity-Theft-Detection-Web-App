955c1debe1a0f52373d65f97285256787e55418d
Ato Toffah <atotoffah@gmail.com>
2023-07-18 10:10:24 +0000

    Added database functions

--- dependencies/functions.py
+++ dependencies/functions.py
@@ -1,8 +1,36 @@
-import pandas as pd
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv, find_dotenv
 import streamlit as st
 
-@st.cache_data(show_spinner=False)
-def read_dataset(path:str):
-    df = pd.read_csv(path)
+
+
+def connectToDatabase():
+    load_dotenv()
+    uri = f"mongodb+srv://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@cluster0.cs7rqja.mongodb.net/?retryWrites=true&w=majority"
+
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
     
-    return df
+
+    return client['electricity_usage']
+
+@st.spinner("Submitting...")
+def submitReport(customerID, predictedValue, actualValue):
+    database = connectToDatabase()
+    reports = database['reports']
+    
+    report = {
+        'CONS_NO': customerID,
+        'Predicted': predictedValue,
+        'Actual': actualValue
+    }
+    
+    reports.insert_one(report)
